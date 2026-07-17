e34880d8c541a2f4973559b4656376d1af7ee28c
DJisaiah <dariesisaiah@gmail.com>
2026-07-18 01:32:16 +0200

    continued with navbar. started with home page

--- frontend/src/components/PomoNavbar.ts
+++ frontend/src/components/PomoNavbar.ts
@@ -2,13 +2,13 @@ export class PomoNavbar extends HTMLElement {
   constructor() {
     super();
   }
-  // need to look into if, an object of this class is made first, so i can maniuplate instance variables for setting style to things like ol
+
   connectedCallback() {
+    this.attachShadow({ mode: "open" });
     const navbar = document.createElement("nav");
     const pageListing = document.createElement("ol");
     const options = {
       Home: "link",
-      "Try Pomo-Tracker!": "link",
       "Submit an Issue": "link",
       About: "link",
     };
@@ -21,10 +21,43 @@ export class PomoNavbar extends HTMLElement {
       page.appendChild(link);
     }
     navbar.appendChild(pageListing);
-    this.appendChild(navbar);
+    this.setStyle();
+    this.shadowRoot!.appendChild(navbar);
   }
 
-  setStyle() {}
+  setStyle() {
+    const sheet = new CSSStyleSheet();
+    sheet.replaceSync(`
+        nav {
+            border-bottom: 2px solid #616161;
+            box-shadow: 1px 1px 1px #212121;
+        }
+        ol {
+            list-style: none;
+            display: flex;
+            justify-content: center;
+        }
+        ol li a {
+            display: inline-block;
+            padding: 2px 25px;
+            text-decoration: none;
+            color: #616161;
+            font-family: sans-serif;
+            font-size: 0.9rem;
+            font-weight: 600;
+        }
+        ol:hover li a {
+            transform: scale(0.9);
+        }
+        ol:hover li a:hover {
+            /*text-decoration: #388e3c wavy underline; */
+            transform: scale(1.5);
+            font-weight: 700;
+            color: #a5d6a7;
+        }
+    `);
+    this.shadowRoot!.adoptedStyleSheets = [sheet];
+  }
 }
 
 customElements.define("pomo-navbar", PomoNavbar);
